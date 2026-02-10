56c022dd17eb68cd137ae985448809d1a847f38b
IT-YorozuBR <it@yorozu.com.br>
2026-02-10 07:34:56 -0300

    permitindo acessar a api de qualquer origem

--- src/server.js
+++ src/server.js
@@ -8,9 +8,16 @@ async function startServer() {
     const app = express();
     const port = process.env.PORT || 3001;
 
-    // Configuração CORS
-    app.use(cors());
+    // const cors = require('cors');
 
+    // app.use(cors());
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        next();
+    });
 
     app.use(express.json({ limit: '50mb' }));
     app.use(express.urlencoded({ extended: true, limit: '50mb' }));
